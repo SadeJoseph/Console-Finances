d1f494b43beb7f5b8104c73d14c4462ee55c3c91
SadeJoseph <feojoseph1@gmail.com>
2023-02-16 15:39:02 +0000

    finance array looopss

--- Console-Finances/starter/file.js
+++ Console-Finances/starter/file.js
@@ -8,3 +8,24 @@
  var change =0 
 
  var totalMonths
+
+
+ for (var main_index =0; main_index <finances.length;main_index ++) {
+    
+    var netProfitLoss = finances[main_index] [1]
+// find greatest profit decrease 
+    if (netProfitLoss <greatestDescrease) {
+        greatestIncrease =netProfitLoss;
+        var greatestMonthDec = finances [main_index][0]
+    }
+    // to find highest prof increase 
+    if (netProfitLoss> greatestIncrease) {
+        greatestIncrease =netProfitLoss;
+        var greatestMonthInc =finances [main_index][0]
+    }
+    var previousdifference =0
+    var change = netProfitLoss - previousdifference
+    previousdifference =change
+    netChange +=change 
+    total+=netProfitLoss
+ }
